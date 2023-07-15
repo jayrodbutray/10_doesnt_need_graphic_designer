ee4ead88bbedcff07aef539094f5f29e71b57907
JayRodButray <jayrodbutray@gmail.com>
2023-07-15 16:10:12 -0700

    getting shapes code

--- index.js
+++ index.js
@@ -1,30 +1,82 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const {Triangle, Circle, Square} = require('./lib/shapes.js');
 
+/*inquirer
+  .prompt([
+    {
+      type: 'list',
+      name: 'shape',
+      message: 'What shape do you want to create?',
+      choices: ['Triangle', 'Circle', 'Square'],
+    },
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Enter a color for your shape:',
+    },
+    {
+      type: 'input',
+      name: 'text',
+      message: 'What three letters do you want for your logo?',
+    },
+  ])
+  .then((answers) => {
+    let shape;
+    switch (answers.shape) {
+      case "Triangle":
+        shape = new Triangle(answers.color);
+        break;
+      case "Circle":
+        shape = new Circle(answers.color);
+        break;
+      case "Square":
+        shape = new Square(answers.color);
+        break;
+    }
+  const svg = shape.render().replace('SVT_TEXT', answers.text);
+
+  fs.writeFileSync('examples/logo.svg', svg);
+  });
+*/
 inquirer
   .prompt([
     {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?',
+      type: "list",
+      name: "shape",
+      message: "Which shape do you want to create?",
+      choices: ["Triangle", "Circle", "Square"],
     },
     {
-      type: 'checkbox',
-      message: 'What languages do you know?',
-      name: 'stack',
-      choices: ['HTML', 'CSS', 'JavaScript', 'MySQL'],
+      type: "input",
+      name: "color",
+      message: "Enter a color for the shape:",
     },
     {
-      type: 'list',
-      message: 'What is your preferred method of communication?',
-      name: 'contact',
-      choices: ['email', 'phone', 'telekinesis'],
+      type: "input",
+      name: "text",
+      message: "Enter the text for your logo:",
     },
   ])
-  .then((data) => {
-    const filename = `${data.name.toLowerCase().split(' ').join('')}.json`;
+  .then((answers) => {
+    // Create the appropriate shape based on the user's choice
+    let shape;
+    switch (answers.shape) {
+      case "Triangle":
+        shape = new Triangle(answers.color);
+        break;
+      case "Circle":
+        shape = new Circle(answers.color);
+        break;
+      case "Square":
+        shape = new Square(answers.color);
+        break;
+    }
 
-    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
-      err ? console.log(err) : console.log('Success!')
-    );
+    // Generate the SVG string
+    
+
+    // Write the SVG string to a file
+    fs.writeFileSync("examples/logo.svg", svg);
   });
+
